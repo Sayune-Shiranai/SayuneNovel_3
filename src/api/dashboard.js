6a863f2d71b46ccdd0bb897ab5ca944bae7111f3
SayuneShiranai <sayuneshiranai1404@gmail.com>
2025-05-26 03:04:09 +0700

    Sayune11

--- src/api/dashboard.js
+++ src/api/dashboard.js
@@ -1,7 +1,8 @@
 // const dashboard = require("../Dashboard");
 
 module.exports = async function (fastifyApp, option) {
-  fastifyApp.get("/dashboard", function (req, rep) {
-    rep.render("dashboard/dashboard");
+  fastifyApp.get("/dashboard", async function (req, rep) {
+    const users = await this.mongo.db.collection("users").find({}).toArray();
+    return rep.render("dashboard/dashboard", { users });
   });
 };
--- src/api/member.js
+++ src/api/member.js
@@ -1,6 +1,7 @@
 //src/api/member.js
 module.exports = async function (fastifyApp, options) {
-  fastifyApp.get("/member", function (req, rep) {
-    rep.render("dashboard/member");
+  fastifyApp.get("/member", async function (req, rep) {
+    const users = await this.mongo.db.collection("users").find({}).toArray();
+    return rep.render("dashboard/dashboard", { users });
   });
 };
