15e9a9130274a7621a02933182d3463878cd2c15
SayuneShiranai <sayuneshiranai1404@gmail.com>
2025-06-20 11:56:37 +0700

    Sayune24

--- src/api/dashboard.js
+++ src/api/dashboard.js
@@ -1,7 +1,18 @@
+const { mongodb } = require("@fastify/mongodb");
+const { ObjectId } = require("@fastify/mongodb");
+
 module.exports = async function (fastifyApp, option) {
   fastifyApp.get("/dashboard", async function (req, rep) {
+    const users = await this.mongo.db.collection("users").find({}).toArray();
+    const user = await this.mongo.db
+      .collection("users")
+      .findOne({ _id: new ObjectId(req.params.id) });
+    const libary = await this.mongo.db.collection("Libary").find({}).toArray();
     return rep.render("dashboard/dashboard", {
-      partial: "member",
+      libary,
+      users,
+      user,
+      partial: "libary",
     });
   });
 };
--- src/api/item.js
+++ src/api/item.js
@@ -1,4 +1,10 @@
 module.exports = async function (fastifyApp, options) {
+  function removeAccents(str) {
+    return str
+      .normalize("NFD") // Tách các ký tự có dấu thành tổ hợp ký tự không dấu + dấu
+      .replace(/[\u0300-\u036f]/g, ""); // Loại bỏ dấu
+  }
+
   function createSlug(itemname) {
     return removeAccents(itemname)
       .toLowerCase()
@@ -14,30 +20,62 @@ module.exports = async function (fastifyApp, options) {
     });
   });
 
-  //router(post/admin/create-item): create a new item
-  // fastifyApp.post("/dashboard/add-item", async function (req, rep) {
-  //   try {
-  //     await pump(
-  //       req.body.imgavt.toBuffer(),
-  //       fs.createWriteStream(
-  //         path.join(__dirname, "public/avt-item", req.body.imgavt.filename)
-  //       )
-  //     );
-  //     // save req.body --> mongodb studentdb
-  //     const slug = createSlug(req.body.itemname.value);
-  //     const result = await this.mongo.db.collection("libary").insertOne({
-  //       itemname: req.body.itemname.value,
-  //       slug: slug,
-  //       theloai: req.body.theloai.value,
-  //       tacgia: req.body.tacgia.value,
-  //       noidung: req.body.noidung.value,
-  //       imgavt: req.body.imgavt.filename,
-  //       createdDate: Date.now(),
-  //     });
+  // router(post/admin/create-item): create a new item
+  fastifyApp.post("/dashboard/add-item", async function (req, rep) {
+    try {
+      //   await pump(
+      //     req.body.imgavt.toBuffer(),
+      //     fs.createWriteStream(
+      //       path.join(__dirname, "public/avt-item", req.body.imgavt.filename)
+      //     )
+      //   );
+      // save req.body --> mongodb studentdb
+      const slug = createSlug(req.body.itemname.value);
+      const item = await this.mongo.db.collection("Libary").insertOne({
+        itemname: req.body.itemname.value,
+        slug: slug,
+        theloai: req.body.theloai.value,
+        tacgia: req.body.tacgia.value,
+        trangthai: req.body.trangthai.value,
+        noidung: req.body.noidung.value,
+        // imgavt: req.body.imgavt.filename,
+        createdDate: Date.now(),
+      });
 
-  //     rep.redirect("/dashboard");
-  //   } catch (err) {
-  //     rep.send(err);
-  //   }
-  // });
+      rep.redirect("/dashboard");
+    } catch (err) {
+      rep.send(err);
+    }
+  });
+
+  fastifyApp.get("/dashboard/update/:id", async function (req, rep) {
+    const user = await this.mongo.db
+      .collection("users")
+      .findOne({ _id: new ObjectId(req.params.id) });
+    return rep.render("dashboard/dashboard", {
+      user,
+      partial: "update-item",
+    });
+  });
+
+  fastifyApp.post("/dashboard/update/:id", async function (req, rep) {
+    const result = await this.mongo.db.collection("users").updateOne(
+      { _id: new ObjectId(req.params.id) },
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          role: req.body.role,
+        },
+      }
+    );
+    rep.redirect("/dashboard");
+  });
+
+  fastifyApp.get("/dashboard/delete/:id", async function (req, rep) {
+    const result = await this.mongo.db
+      .collection("users")
+      .deleteOne({ _id: new ObjectId(req.params.id) });
+    rep.redirect("/dashboard");
+  });
 };
